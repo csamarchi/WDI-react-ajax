b758bb7d83d9335633348ab3e42c801e65735494
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-10-31 10:18:25 -0500

    created and connected Login and MainContainer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+import MainContainer from './MainContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      logged: false,
+      username: ''
+    }
+  }
+
+  handleLogin = (username, logged) => {
+    this.setState({
+      username: username,
+      logged: logged
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {this.state.logged ? <MainContainer username={this.state.username} /> : <Login handleLogin={this.handleLogin} />}
       </div>
     );
   }
---
+++ src/MainContainer/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+ class MainContainer extends Component {
+	constructor(){
+		super();
+		this.state = {
+ 		}
+	}
+	render(){
+		return (
+			<h1>MainContainer</h1>
+		)
+	}
+}
+ export default MainContainer;
